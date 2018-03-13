a61906fa958721ddfd51ff29ee23a5266a7f5891
David Serna <davidserna08@gmail.com>
2018-03-12 23:59:03 -0300

    Cambios generales
    Cambio el menu de navegacion, tanto en estructura como en estilo.
    Cambio en la forma en la que se ven las imagenes, se continua usando flexbox para el display, pero se cambia el plugin de la galeria por lightbox, aun existen errores en la funcion pero en general funciona bien

--- static/js/bits.js
+++ static/js/bits.js
@@ -1,94 +1,52 @@
+// Open the Modal
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+var slideIndex = 0;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
 
 
 $(document).ready(function(){
-	var pswpElement = document.querySelectorAll('.pswp')[0];
 
-	// build items array
-	var items = [
-	    {
-	        src: 'static/img/1_Recuerdos_Del_Pasado.jpg',
-	        w: 600,
-	        h: 400,
-	        msrc: 'static/img/1_Recuerdos_Del_Pasado.jpg', 
-	        title: 'Recuerdos del pasado'  
-	    },
-	    {
-	        src: 'static/img/2_Brisas_Marinas.jpg',
-	        w: 600,
-	        h: 400,
-	        msrc: 'static/img/1_Recuerdos_Del_Pasado.jpg', 
-	        title: 'Brisas marinas' 
-	    },
-	    {
-	        src: 'static/img/3_Sol_De_Medio_Dia.jpg',
-	        w: 600,
-	        h: 400,
-	        msrc: 'static/img/1_Recuerdos_Del_Pasado.jpg', 
-	        title: 'Sol de medio dia' 
-	    },
-	    {
-	        src: 'static/img/4_Otoño.jpg',
-	        w: 600,
-	        h: 400,
-	        msrc: 'static/img/1_Recuerdos_Del_Pasado.jpg', 
-	       	title: 'Otoño' 
-	    },
-	    {
-	        src: 'static/img/5_Sorpresas_De_La_Vida.jpg',
-	        w: 600,
-	        h: 400,
-	        msrc: 'static/img/1_Recuerdos_Del_Pasado.jpg', 
-	       	title: 'Sorpresas de la vida' 
-	    },
-	    {
-	        src: 'static/img/6_Alma_Vibrante.jpg',
-	        w: 600,
-	        h: 400,
-	        msrc: 'static/img/1_Recuerdos_Del_Pasado.jpg', 
-	       	title: 'Alma vibante' 
-	    },
-	    {
-	        src: 'static/img/7_Tiempos.jpg',
-	        w: 600,
-	        h: 400,
-	        msrc: 'static/img/1_Recuerdos_Del_Pasado.jpg', 
-	       	title: 'Tiempos' 
-	    },
-	    {
-	        src: 'static/img/8_Ambiente.jpg',
-	        w: 600,
-	        h: 400,
-	        msrc: 'static/img/1_Recuerdos_Del_Pasado.jpg', 
-	       	title: 'Ambiente' 
-	    },
-	    {
-	        src: 'static/img/9_Vision_Interior.jpg',
-	        w: 600,
-	        h: 400,
-	        msrc: 'static/img/1_Recuerdos_Del_Pasado.jpg', 
-	        title: 'Vision interior' 
-	    },
-	    {
-	        src: 'static/img/10_Naturaleza.jpg',
-	        w: 600,
-	        h: 400,
-	        msrc: 'static/img/1_Recuerdos_Del_Pasado.jpg', 
-	        title: 'Naturaleza' 
-	    },
-	    
-
-	];
-
-	// define options (if needed)
-	var options = {
-	    // optionName: 'option value'
-	    // for example:
-	    index: 0 // start at first slide
-	};
-
-	// Initializes and opens PhotoSwipe
-	var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
-	//gallery.init();
+	
 });
 
 
